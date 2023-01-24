bc6aec78b29e5a71d11bbe47260b9f221ed2538d
joaoportes <joaovitor.portes06@gmail.com>
2023-01-24 20:54:35 -0300

    yes git push

---
+++ projeto.RNG/script.js
@@ -0,0 +1,16 @@
+const button = document.getElementById('button-generate');
+
+const generate = () => {
+    const numParagrafo = document.getElementById('num-paragrafo');
+    const inputMin = parseInt( document.getElementById('input-min').value);
+    const inputMax = parseInt( document.getElementById('input-max').value);
+    
+    numParagrafo.innerHTML = Math.floor(Math.random() * (inputMax - inputMin + 1)) + inputMin;
+    
+}
+
+button.addEventListener('click', generate)
+
+
+
+
